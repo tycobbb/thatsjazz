206b2724b630d95dc5ee416852f3cc81842a7a02
Ty Cobb <ty.cobb.m@gmail.com>
2021-10-03 13:10:45 -0400

    add squish

---
+++ Assets/ThatsJazz/Core/Linear.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+/// a value that can be lerp'd
+[Serializable]
+public struct Linear<T> {
+    // -- props --
+    [Tooltip("the destination value.")]
+    public T Dst;
+
+    /// the time value, interpretable in a variety of ways
+    [Tooltip("the time value. interpretation is context-dependent.")]
+    public float Time;
+
+    // -- lifetime --
+    /// create a new linear value
+    public Linear(T dst, float time) {
+        Dst = dst;
+        Time = time;
+    }
+}
--- Assets/ThatsJazz/Player/Player.cs
+++ Assets/ThatsJazz/Player/Player.cs
@@ -7,8 +7,23 @@ public class Player: MonoBehaviour {
     [Tooltip("the magnitude of the move acceleration")]
     [SerializeField] float mMoveMag = 2.0f;
 
+    [Tooltip("the scale when normal. time is duration.")]
+    [SerializeField] Linear<Vector3> mScaleDefault = new Linear<Vector3>(
+        dst: Vector3.one,
+        time: 3.0f
+    );
+
+    [Tooltip("the scale when squished. time is duration.")]
+    [SerializeField] Linear<Vector3> mScaleSquished = new Linear<Vector3>(
+        dst: new Vector3(1.2f, 0.7f, 1.2f),
+        time: 1.0f
+    );
+
     // -- nodes --
     [Header("nodes")]
+    [Tooltip("the player's root transform")]
+    [SerializeField] Transform mRoot;
+
     [Tooltip("the player's foot's rigidbody")]
     [SerializeField] Rigidbody mFoot;
 
@@ -25,6 +40,10 @@ public class Player: MonoBehaviour {
     /// the squish input
     InputAction mSquish;
 
+    // -- props --
+    /// the current squish velocity
+    Vector3 mSquishVel = Vector3.zero;
+
     // -- lifecycle --
     void Awake() {
         // find inputs
@@ -34,20 +53,44 @@ public class Player: MonoBehaviour {
     }
 
     void FixedUpdate() {
+        // squish the player
+        Squish();
+
         // move the player
         Move();
     }
 
     // -- commands --
-    /// move the character
+    /// move the player
     void Move() {
-        var pos = mMove.ReadValue<Vector2>();
-        var dir = new Vector3(pos.x, 0.0f, pos.y);
+        var input = mMove.ReadValue<Vector2>();
+        var dir = new Vector3(input.x, 0.0f, input.y);
 
         mFoot.AddForceAtPosition(
             dir * mMoveMag,
             mMovePos.position,
             ForceMode.Acceleration
         );
+    }
+
+    /// squish the player, flattening them and increasing their surface area
+    void Squish() {
+        var scale = mScaleDefault;
+
+        // if squished, target that scale
+        if (mSquish.IsPressed()) {
+            scale = mScaleSquished;
+        }
+
+        // animate towards scale
+        var next = Vector3.SmoothDamp(
+            mRoot.localScale,
+            scale.Dst,
+            ref mSquishVel,
+            scale.Time
+        );
+
+        // update scale
+        mRoot.localScale = next;
     }
 }
